e41a54fc4fe932a9ab2d58a9c5f145ba57d7d76a
Bryan Hurst <bdhurst@mchsi.com>
2022-08-30 23:08:45 -0500

    Faulty Save

--- Assets/Scripts/Unit Based Scripts/AI/BasicEnemy/AI_Grunt.cs
+++ Assets/Scripts/Unit Based Scripts/AI/BasicEnemy/AI_Grunt.cs
@@ -5,10 +5,18 @@ using UnityEngine.AI;
 
 public class AI_Grunt : Unit_AI_Base, Unit_AI_Brain
 {
+    public float meleeRange = 2;
+    public float meleeRangeTolerance = 3;
+    public float rangedRange = 15;
+    public float rangeRangeTolerance = 20;
+    public float stopPursueRange = 25;
+    public float leashRange = 25;
+
+    public float aggroRange = 20; //Distance at which unit will decide to engage into combat
+
     private void Start()
     {
         InitializeAI();
-        agent.speed = 8.4315f;
         wanderInterval = Random.Range(1.5f, 3.5f);
     }
 
@@ -17,20 +25,57 @@ public class AI_Grunt : Unit_AI_Base, Unit_AI_Brain
         return unit.knownAbilities[0];
     }
 
+    public void Pursue()
+    {
+        CheckRange(meleeRange, meleeRangeTolerance, rangedRange, rangeRangeTolerance, stopPursueRange);
+        var chaseDistance = Vector3.Distance(transform.position, lastPositionBeforeCombat);
+
+        if (rangeState != RangeState.OutOfPursueRange && chaseDistance < leashRange)
+        {
+            if (UtilityService.LineOfSightCheckRootUnit(unit.transform.position + unit.eyesOffset, unit.currentTarget) != new Vector3() && rangeState >= desiredRangeState && !RootAbility.NullorUninitialized(unit.abilityPreparingToCast))
+            {
+                FaceTarget();
+                unit.currentTargetPoint = unit.currentTarget.transform.position;
+                unit.ActiveAbilityCheck();
+            }
+            else if (UtilityService.LineOfSightCheckRootUnit(unit.transform.position + unit.eyesOffset, unit.currentTarget) != new Vector3() && rangeState >= desiredRangeState)
+            {
+                switch (desiredRangeState)
+                {
+                    case RangeState.Ranged:
+                        agent.stoppingDistance = meleeRange;
+                        break;
+                    case RangeState.Melee:
+                        agent.stoppingDistance = rangedRange;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else
+            {
+                unit.abilityPreparingToCast = null;
+                unit.currentCastingTime = 0;
+                agent.destination = unit.currentTarget.transform.position;
+            }
+        }
+        else
+        {
+            StopPursue();
+        }
+    }
+
     public override void Tick()
     {
         if (unit.isAlive == true)
         {
             if (actionState == AIActionState.Patrolling)
             {
-                if (unit.currentTarget == null)
-                {
-                    Patrol();
-                    if (unit.hostility == Hostility.Hostile)
-                        FindTarget();
-                }
+                Patrol();
+                if (unit.hostility == Hostility.Hostile)
+                    FindTargetInRange(aggroRange);
             }
-            else if(actionState == AIActionState.Attacking)
+            else if (actionState == AIActionState.Attacking)
             {
                 Pursue();
             }
--- Assets/Scripts/Unit Based Scripts/AI/Unit_AI_Base.cs
+++ Assets/Scripts/Unit Based Scripts/AI/Unit_AI_Base.cs
@@ -8,22 +8,25 @@ public class Unit_AI_Base : MonoBehaviour
     public NPCUnit unit;
     public NavMeshAgent agent;
     public AIActionState actionState = AIActionState.Idle;
+    public RangeState rangeState = RangeState.OutOfPursueRange;
+    public RangeState desiredRangeState = RangeState.OutOfPursueRange;
 
     public List<Transform> patrolWaypoints = new List<Transform>();
     public int waypointListIndex = 0;
+    public float patrolSpeed = 6;
+    public float wanderSpeed = 2;
     public float wanderTimer = 0;
     public float wanderInterval = 2;
     public int timesToWanderAtWaypoints = 1;
-    public Vector3 lastIdleDestination;
-
-    public bool aggroed = false;
-    public float minPreferredDistance = 15; // Distance at which unit will attempt to attack
-    public float maxPreferredDistance = 20; // Distance at which unit will STOP attempting to attack and will attempt to get back to the minPreferredDistance
+    public Vector3 lastPatrolDestination;
+    public Vector3 lastPositionBeforeCombat;
+    public bool inCombat = false;
 
     public void InitializeAI()
     {
         agent = GetComponent<NavMeshAgent>();
         unit = GetComponent<NPCUnit>();
+        agent.speed = patrolSpeed;
     }
 
     public void GoToNextPoint()
@@ -36,6 +39,7 @@ public class Unit_AI_Base : MonoBehaviour
         if (waypointListIndex == patrolWaypoints.Count)
             waypointListIndex = 0;
         agent.destination = patrolWaypoints[waypointListIndex].position;
+        lastPatrolDestination = patrolWaypoints[waypointListIndex].position;
     }
 
     public void FaceTarget()
@@ -49,38 +53,22 @@ public class Unit_AI_Base : MonoBehaviour
         return null;
     }
 
-    public void Pursue()
+    public void CheckRange(float meleeRange, float meleeRangeTolerance, float rangedRange, float rangedRangeTolerance, float stopPursueRange)
     {
-        var targetDistance = Vector3.Distance(transform.position, unit.currentTarget.transform.position);
-        var retreatDistance = Vector3.Distance(transform.position, lastIdleDestination);
+        float targetDistance = Vector3.Distance(transform.position, unit.currentTarget.transform.position);
 
-        if (targetDistance < 25 && retreatDistance < 25)
-        {
-            if(UtilityService.LineOfSightCheckRootUnit(unit.transform.position + unit.eyesOffset, unit.currentTarget) != new Vector3() && targetDistance <= maxPreferredDistance && !RootAbility.NullorUninitialized(unit.abilityPreparingToCast))
-            {
-                FaceTarget();
-                unit.currentTargetPoint = unit.currentTarget.transform.position;
-                unit.ActiveAbilityCheck();
-            }
-            else if (UtilityService.LineOfSightCheckRootUnit(unit.transform.position + unit.eyesOffset, unit.currentTarget) != new Vector3() && targetDistance <= minPreferredDistance)
-            {
-                if (RootAbility.NullorUninitialized(unit.abilityPreparingToCast))
-                {
-                    unit.abilityPreparingToCast = DetermineAbilityToUse();
-                    agent.stoppingDistance = minPreferredDistance;
-                }
-            }
-            else
-            {
-                unit.abilityPreparingToCast = null;
-                unit.currentCastingTime = 0;
-                agent.destination = unit.currentTarget.transform.position;
-            }
-        }
+        if (targetDistance < meleeRange)
+            rangeState = RangeState.Melee;
+        else if (targetDistance < meleeRangeTolerance)
+            rangeState = RangeState.Melee_Tolerance;
+        else if (targetDistance < rangedRange)
+            rangeState = RangeState.Ranged;
+        else if (targetDistance < rangedRangeTolerance)
+            rangeState = RangeState.Ranged_Tolerance;
+        else if (targetDistance < stopPursueRange)
+            rangeState = RangeState.OutOfAttackRange;
         else
-        {
-            StopPursue();
-        }
+            rangeState = RangeState.OutOfPursueRange;
     }
 
     public void StopPursue()
@@ -90,8 +78,10 @@ public class Unit_AI_Base : MonoBehaviour
         unit.abilityPreparingToCast = null;
         unit.currentTarget = null;
         agent.isStopped = false;
-        agent.destination = lastIdleDestination;
-        aggroed = false;
+        agent.speed = patrolSpeed;
+        agent.stoppingDistance = 0;
+        agent.destination = lastPatrolDestination;
+        inCombat = false;
     }
 
     public void Patrol()
@@ -103,6 +93,7 @@ public class Unit_AI_Base : MonoBehaviour
             {
                 if (timesToWanderAtWaypoints > 0)
                 {
+                    agent.speed = wanderSpeed;
                     wanderInterval = Random.Range(1.5f, 3.5f);
                     agent.destination = patrolWaypoints[waypointListIndex].position + new Vector3(Random.Range(-3.5f, 3.5f), 0, Random.Range(-3.5f, 3.5f));
                     timesToWanderAtWaypoints--;
@@ -110,6 +101,7 @@ public class Unit_AI_Base : MonoBehaviour
                 }
                 else
                 {
+                    agent.speed = patrolSpeed;
                     waypointListIndex++;
                     GoToNextPoint();
                     timesToWanderAtWaypoints = Random.Range(1, 3);
@@ -119,14 +111,25 @@ public class Unit_AI_Base : MonoBehaviour
         }
     }
 
-    public void FindTarget()
+    public void FindTargetInRange(float range)
     {
-        unit.currentTarget = GameWorldReferenceClass.GetInAreaPlayer(20, transform.position);
+        unit.currentTarget = GameWorldReferenceClass.GetInAreaPlayer(range, transform.position);
         if (unit.currentTarget != null)
         {
-            aggroed = true;
+            inCombat = true;
             actionState = AIActionState.Attacking;
-            lastIdleDestination = transform.position;
+            agent.speed = unit.totalStats.MovementSpeed_Current;
+        }
+    }
+
+    public void FindTargetInVision(float range, float visionAngle)
+    {
+        unit.currentTarget = GameWorldReferenceClass.GetInAreaPlayer(range, transform.position);
+        if (unit.currentTarget != null)
+        {
+            inCombat = true;
+            actionState = AIActionState.Attacking;
+            agent.speed = unit.totalStats.MovementSpeed_Current;
         }
     }
 
@@ -145,5 +148,16 @@ public class Unit_AI_Base : MonoBehaviour
         Idle,
         Patrolling,
         Attacking
+    }
+
+    //Each section is within the previos one
+    public enum RangeState
+    {
+        OutOfPursueRange,
+        OutOfAttackRange,
+        Ranged_Tolerance,
+        Ranged,
+        Melee_Tolerance,
+        Melee
     }
 }
